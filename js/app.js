32f8807b8998c04c725190a9b3eaa9dafc648110
Gaston Amigo <gastonamigo@gmail.com>
2022-09-07 21:23:15 -0300

    se agrego api de dolar

--- js/app.js
+++ js/app.js
@@ -2,25 +2,27 @@
 
 const listaProductos = document.getElementById('listaProductos');
 const contenedorCarrito = document.getElementById('carrito');
-const botonVaciarCarrito= document.getElementById('vaciarCarrito');
+const botonVaciarCarrito = document.getElementById('vaciarCarrito');
 const contadorCarrito = document.getElementById('contadorCarrito');
 const precioTotal = document.getElementById('precioTotal');
 let carrito = [];
 
-document.addEventListener('DOMContentLoaded', () =>{
-    if(localStorage.getItem('carrito')){
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('carrito')) {
         carrito = JSON.parse(localStorage.getItem('carrito'));
         actualizarCarrito();
     }
 })
 
-botonVaciarCarrito.addEventListener('click',()=> {
+botonVaciarCarrito.addEventListener('click', () => {
     localStorage.removeItem('carrito');
     carrito = [];
     actualizarCarrito();
 })
 
-productos.forEach((producto)=>{
+productos.forEach((producto) => {
     const card = document.createElement('card');
     card.classList.add('producto');
     card.innerHTML = `
@@ -35,60 +37,77 @@ productos.forEach((producto)=>{
            </div>
          </div>
      </div>`;
-     listaProductos.appendChild(card);
-     const botonId = `agregar${producto.id}`;
-     const boton = document.getElementById(botonId);
-     
-     
-     boton.addEventListener('click', () => {
+    listaProductos.appendChild(card);
+    const botonId = `agregar${producto.id}`;
+    const boton = document.getElementById(botonId);
+
+
+    boton.addEventListener('click', () => {
         Toastify({
 
             text: `Agregaste al Carrito 1 ${producto.nombre}`,
-            
+
             duration: 3000,
-            
-            gravity:'top',
 
-            position:'right',
+            gravity: 'top',
 
-            }).showToast();
+            position: 'right',
+
+        }).showToast();
         agregarAlcarrito(producto.id);
-     })
+    })
 })
-const quitarDelCarrito = (prodId)=> {
-    const item = carrito.find((prod)=> prod.id === prodId);
+const quitarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id === prodId);
     const indice = carrito.indexOf(item);
     carrito.splice(indice, 1);
     localStorage.removeItem('carrito');
     actualizarCarrito();
 }
 
-const agregarAlcarrito = (prodId) =>{
-    const existe = carrito.some (prod => prod.id === prodId);
-    if(existe){
+const agregarAlcarrito = (prodId) => {
+    const existe = carrito.some(prod => prod.id === prodId);
+    if (existe) {
         const prod = carrito.map(prod => {
             prod.id === prodId && prod.cantidad++;  // operador logico and
         })
     } else {
-    const item = productos.find ((prod) => prod.id === prodId);
-    carrito.push(item);
-    console.log(carrito);
+        const item = productos.find((prod) => prod.id === prodId);
+        carrito.push(item);
+        console.log(carrito);
     }
     actualizarCarrito();
 }
 const actualizarCarrito = () => {
     contenedorCarrito.innerHTML = "";
-    carrito.forEach((prod) =>{
-    const dropdown = document.createElement('dropdown');
-    dropdown.innerHTML = `
+    carrito.forEach((prod) => {
+        const dropdown = document.createElement('dropdown');
+        dropdown.innerHTML = `
     <a class="dropdown-item">Cantidad ${prod.cantidad} -- ${prod.nombre} ---- $${prod.precio}<button onclick="quitarDelCarrito(${prod.id})" class="btn buttonQuitar">Quitar</button></a>
      `;
-     contenedorCarrito.appendChild(dropdown);
-     localStorage.setItem('carrito', JSON.stringify(carrito));
-     console.log(...carrito); //Este spread es solo para probar este operador
+        contenedorCarrito.appendChild(dropdown);
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+        console.log(...carrito); //Este spread es solo para probar este operador
     })
     //contadorCarrito.innerText = carrito.length;
-    precioTotal.innerText = carrito.reduce((acc, prod)=> acc + (prod.cantidad * prod.precio), 0); // reset parameters
+    let total = carrito.reduce((acc, prod) => acc + (prod.cantidad * prod.precio), 0); // reset parameters
+    precioTotal.innerText = total;
+    
+    // Precio Dolar API ====================================================
+    let totalUsd = document.getElementById('totalUsd');
+    let dolarblue = 0;
+    fetch('https://api.bluelytics.com.ar/v2/latest')
+        .then((response) => response.json())
+        .then((a) => {
+            dolarblue = parseFloat(a['blue'].value_sell);
+        })
+        .catch(() => console.log('no se puede obtener el valor en USD'));
+
+    setTimeout(() => {
+
+        totalUsd.innerHTML = `: ${(total / dolarblue).toFixed(2)}<div>Cotizacion Dólar (1USD = $${dolarblue})</div>`;
+
+    }, 1000)
 }
 
 
